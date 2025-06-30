2936568dd46f98acf0832c2e427f18dc606f0401
franco <franconicolas.pfahler@estudiantes.unahur.edu.ar>
2025-06-30 20:49:25 -0300

    Integrado Plus

---
+++ UnaHur-Anti-Social-Net-Interfaz-de-Usuario/src/components/VisualAlert.jsx
@@ -0,0 +1,11 @@
+import '../styles/VisualAlert.css';
+
+function VisualAlert({ mensaje }) {
+  return (
+    <div className="alert">
+      {mensaje}
+    </div>
+  );
+}
+
+export default VisualAlert;
